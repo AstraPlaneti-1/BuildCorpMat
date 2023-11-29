b641393fd095172191622501d597fda56a0c8666
DrPepper05 <bogalima6@gmail.com>
2023-11-29 19:00:43 +0200

    link fix

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,45 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'lucas.rus.dojo@gmail.com';
+$name = $_POST["name"];
+$email = $_POST["email"];
+$subject = $_POST["subject"];
+$message = $_POST["message"];
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+require '../phpmailer/src/Exception.php';
+require '../phpmailer/src/PHPMailer.php';
+require '../phpmailer/src/SMTP.php';
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+$mail = new PHPMailer(true);
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
 
-  echo $contact->send();
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+$mail->Host = 'smtp.gmail.com';
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = 587;
+$mail->Username = 'lucas.rus.dojo@gmail.com';
+$mail->Password = 'fyslihtgqwbfgawy';
+
+$mail->setFrom($email , "BuildCorpMat"); // Set your Gmail address and your name
+$mail->addAddress("lucas.rus.dojo@gmail.com" , "BuildCorpMat");
+
+
+
+$mail->addReplyTo($email, $name); // Set the reply-to address to the user's email and name
+
+$mail->Subject = "Comanda Site BuildCorpMat";
+$mail->Body = "name:" . $name .  "\n" . "email: " . $email . "\n" . "Telefon:" . $subject . "\n" . "Comanda:" ."\n" . $message;
+
+$mail->send();
+
+echo "
+    <script>
+    alert('Sent');
+    document.location.href = 'contact.html'
+    </script>
+    ";
 ?>
